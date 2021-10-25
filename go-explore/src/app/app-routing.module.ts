002c9c5e38e799b6a9c7087b4c0675c5e8caca88
DangerNoodle32 <71064866+DangerNoodle32@users.noreply.github.com>
2021-10-25 13:29:28 +0200

    login route removed

--- go-explore/src/app/app-routing.module.ts
+++ go-explore/src/app/app-routing.module.ts
@@ -6,10 +6,6 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
-  {
     path: 'registration',
     loadChildren: () => import('./registration/registration.module').then( m => m.RegistrationPageModule)
   },
