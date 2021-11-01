db8fc68c659a776a7dd5c338b30ee31bdfaabc15
DangerNoodle32 <71064866+DangerNoodle32@users.noreply.github.com>
2021-11-01 12:46:48 +0100

    validation added

--- go-explore/src/app/registration/registration.page.ts
+++ go-explore/src/app/registration/registration.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators, PatternValidator } from '@angular/forms';
+
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +9,85 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationPage implements OnInit {
 
-  constructor() { }
+  validationMessages: any;
+  validationForm: FormGroup;
+  request: any = { Language: 'english' };
+  confirm: any;
+
+  constructor(public formBuilder: FormBuilder) { }
   
-  ngOnInit() {
+  ngOnInit() 
+    {
+      this.prepareFormValidation();
+  }
+
+  prepareFormValidation() {
+
+    this.validationForm = this.formBuilder.group({
+      Password: new FormControl('', Validators.compose([
+        Validators.minLength(2),
+        Validators.maxLength(30),
+        Validators.pattern('[a-zA-Z, 0-9 ]*'),
+        Validators.required])),
+        Username: new FormControl('', Validators.compose([
+          Validators.minLength(2),
+          Validators.maxLength(30),
+          Validators.pattern('[a-zA-Z, 0-9 ]*'),
+          Validators.required])),
+          Terms: new FormControl('', Validators.compose([
+            Validators.minLength(2),
+            Validators.maxLength(30),
+            Validators.pattern('[a-zA-Z, 0-9 ]*'),
+            Validators.required])),
+      Email: new FormControl('', Validators.compose([
+        //Validators.email,
+        Validators.pattern(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/),
+        Validators.required
+      ]))
+    });
+
+    this.validationMessages = {
+      
+      'Email': [
+        {
+          type: 'required',
+          message: 'Email is required.'
+        },
+        {
+          type: 'pattern',
+          message: 'Must be a valid email address.'
+        },
+      ],
+
+      'Username': [
+        {
+          type: 'required',
+          message: 'Username is required.'
+        },
+        {
+          type: 'pattern',
+          message: 'Must be a valid username.'
+        },
+      ],
+
+      'Password': [
+        {
+          type: 'required',
+          message: 'Password is required'
+        },
+        {
+          type: 'pattern',
+          message: 'Invalid password'
+        }
+      ]
+    }
+
+  }
+
+  LognIn(values) {
+    this.request = values;
+    console.log(this.request);
+    this.confirm();
   }
 
 }
