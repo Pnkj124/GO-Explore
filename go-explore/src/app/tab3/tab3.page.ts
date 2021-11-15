fa65d22423b1c8a5ae1f90bf3321eb147851320b
paradise98425 <paradise98425@gmail.com>
2021-11-15 11:54:18 +0100

    upload profile picture done

--- go-explore/src/app/tab3/tab3.page.ts
+++ go-explore/src/app/tab3/tab3.page.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {}
+export class Tab3Page {
+  private file: File;
+
+  constructor(private http: HttpClient){}
+
+  onFileChange(fileChangeEvent) {
+    this.file = fileChangeEvent.target.files[0];
+  }
+
+  async submitForm(){
+    let formData = new FormData();
+    formData.append("profile_picture", this.file, this.file.name)
+    this.http.put("http://localhost:3000/api/users/upload", formData).subscribe((response) => {
+      console.log(response);
+    });
+  }
+}
