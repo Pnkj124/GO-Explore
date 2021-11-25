e79e1312cddf931e127898d9381642896f45a5cd
paradise98425 <paradise98425@gmail.com>
2021-11-25 11:33:29 +0100

    changes

--- go-explore/src/app/tab1/tab1.module.ts
+++ go-explore/src/app/tab1/tab1.module.ts
@@ -6,7 +6,6 @@ import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
-
 @NgModule({
   imports: [
     IonicModule,
