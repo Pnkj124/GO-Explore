e3b031abebb532c0ff34c7fd1d0c72fcd6fc9b8e
Pankaj Chaudhary <67068794+Pnkj124@users.noreply.github.com>
2021-11-29 17:15:41 +0100

    fix image upload ui  loading

--- go-explore/src/app/tab3/tab3.page.ts
+++ go-explore/src/app/tab3/tab3.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import { Storage } from '@ionic/storage-angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -20,24 +21,24 @@ export class Tab3Page {
 
   private file: File;
 
+  private baseUrl: string = 'http://localhost:3000'
 
-  constructor(private http: HttpClient, private storage: Storage){}
+  constructor(private http: HttpClient, private storage: Storage, private toastController: ToastController){}
 
   async ngOnInit() {
+
     await this.storage.create();
     const email = await this.storage.get('email')
-    const header = { 'email': email}
-    this.http.get("http://localhost:3000/api/users/profile-picture", {headers: header, responseType: 'blob'}).subscribe((blob : any) => {
-      let objectURL = URL.createObjectURL(blob);
-      this.image = objectURL;
-    });
+    const header = { 'email': email }
 
-    this.http.get("http://localhost:3000/api/users/badges", {headers: header, responseType: 'blob'}).subscribe((blob : any) => {
+    await this.loadProfilePicture(header);
+
+    this.http.get(`${this.baseUrl}/api/users/badges`, {headers: header, responseType: 'blob'}).subscribe((blob : any) => {
       let objectURL = URL.createObjectURL(blob);
       this.badgeImage = objectURL;
     });
 
-    this.http.get("http://localhost:3000/api/users/details", {headers: header}).subscribe((response : any) => {
+    this.http.get(`${this.baseUrl}/api/users/details`, {headers: header}).subscribe((response : any) => {
       this.username = response.username;
     });
   }
@@ -46,14 +47,31 @@ export class Tab3Page {
     this.file = fileChangeEvent.target.files[0];
   }
 
+  async loadProfilePicture(header){
+    this.http.get(`${this.baseUrl}/api/users/profile-picture`, { headers: header, responseType: 'blob' }).subscribe((blob: any) => {
+      let objectURL = URL.createObjectURL(blob);
+      this.image = objectURL;
+    });
+  }
+
   async submitForm(){
     const email = await this.storage.get('email')
     let formData = new FormData();
     formData.append("profile_picture", this.file, this.file.name)
     formData.append("email", email)
-    this.http.put("http://localhost:3000/api/users/upload", formData).subscribe((response) => {
+    this.http.put(`${this.baseUrl}/api/users/upload`, formData).subscribe((response) => {
       console.log(response);
-      window.location.reload();
+
+      const header = { 'email': email };
+
+      this.loadProfilePicture(header);
+  
     });
+
+    const toast = await this.toastController.create({
+      message: 'Profile Updated successfully.',
+      duration: 3000
+    });
+    toast.present();
   }
 }
